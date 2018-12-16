72199945fdc762aa1504082d4d273555d6cb8b20
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2018-12-16 11:54:34 -0800

    Verify code style with Rubocop.

--- lastpass2pass.rb
+++ lastpass2pass.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
-# Copyright (C) 2012 Alex Sayers <alex.sayers@gmail.com>. All Rights Reserved.
-# This file is licensed under the GPLv2+. Please see COPYING for more information.
+# Copyright (C) 2012 Alex Sayers <alex.sayers@gmail.com>.  All Rights Reserved.
+# This file is licensed under the GPLv2+.  Please see COPYING for more
+# information.
 
 # LastPass Importer
 #
@@ -9,27 +10,31 @@
 
 # Usage:
 #
-# Go to lastpass.com and sign in. Next click on your username in the top-right
-# corner. In the drop-down meny that appears, click "Export". After filling in
-# your details again, copy the text and save it somewhere on your disk. Make sure
-# you copy the whole thing, and resist the temptation to "Save Page As" - the
-# script doesn't like HTML.
+# Go to lastpass.com and sign in.  Next click on your username in the top-right
+# corner. In the drop-down menu that appears, click “Export”.  After filling in
+# your details again, copy the text and save it somewhere on your disk.  Make
+# sure you copy the whole thing, and resist the temptation to “Save Page As” —
+# the script doesn’t like HTML.
 #
-# Fire up a terminal and run the script, passing the file you saved as an argument.
-# It should look something like this:
+# Fire up a terminal and run the script, passing the file you saved as an
+# argument.  It should look something like this:
 #
-#$ ./lastpass2pass.rb path/to/passwords_file.csv
+# $ ./lastpass2pass.rb path/to/passwords_file.csv
 
 require 'csv'
+require 'English'
 require 'optparse'
 
 # Parse flags
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: #{$0} [options] filename"
+  opts.banner = "Usage: #{$PROGRAM_NAME} [options] filename"
 
   FORCE = false
-  opts.on("-f", "--force", "Overwrite existing records") { FORCE = true }
-  opts.on("-h", "--help", "Display this screen") { puts opts; exit }
+  opts.on('-f', '--force', 'Overwrite existing records') { FORCE = true }
+  opts.on('-h', '--help', 'Display this screen') do
+    puts opts
+    exit
+  end
 
   opts.parse!
 end
@@ -40,41 +45,45 @@ if ARGV.empty?
   exit 0
 end
 
-# Get filename of csv file
-filename = ARGV.join(" ")
-puts "Reading '#{filename}'..."
+# Get filename of CSV file
+filename = ARGV.join(' ')
+puts "Reading “#{filename}”..."
 
-
+# Represent 1 password from LastPass imported from a LastPass CSV
 class Record
-  def initialize name, url, username, password, extra
-    @name, @url, @username, @password, @extra = name, url, username, password, extra
+  def initialize(name, url, username, password, extra)
+    @name = name
+    @url = url
+    @username = username
+    @password = password
+    @extra = extra
   end
 
   def name
-    s = ""
-    s << "Secure Notes/" if secure_note?
-    s << @name unless @name == nil
-    s.gsub(/'/, "")
+    s = ''
+    s << 'Secure Notes/' if secure_note?
+    s << @name unless @name.nil?
+    s.delete("'")
   end
 
   def to_s
-    s = ""
+    s = ''
     s << "#{@password}\n"
     s << "username: #{@username}\n" unless @username.nil? || @username.empty?
     s << "url: #{@url}\n" unless secure_note?
     s << "#{@extra}\n" unless @extra.nil?
-    return s
+    s
   end
 
   private
 
   def secure_note?
-    @url == "http://sn"
+    @url == 'http://sn'
   end
 end
 
 unless File.exist?(filename)
-  puts "Couldn't find #{filename}!"
+  puts "Couldn’t find “#{filename}”!"
   exit 1
 end
 
@@ -89,7 +98,7 @@ rows.each do |args|
   args.pop # Ignore “fav”
   args.pop # Ignore “grouping”
   name = args.pop
-  extra = args.join(",")
+  extra = args.join(',')
 
   records << Record.new(name, url, username, password, extra)
 end
@@ -98,26 +107,29 @@ puts "Records parsed: #{records.length}"
 successful = 0
 errors = []
 records.each do |r|
-  if File.exist?("#{r.name}.gpg") and FORCE == false
-    puts "skipped #{r.name}: already exists"
+  if File.exist?("#{r.name}.gpg") && FORCE == false
+    puts "Skipped “#{r.name}”: Already exists."
     next
   end
-  print "Creating record #{r.name}..."
+  print "Creating record “#{r.name}”..."
   IO.popen("pass insert -m '#{r.name}' > /dev/null", 'w') do |io|
     io.puts r
   end
-  if $? == 0
-    puts " done!"
+  if $CHILD_STATUS == 0
+    puts ' done!'
     successful += 1
   else
-    puts " error!"
+    puts ' error!'
     errors << r
   end
 end
 puts "#{successful} records successfully imported!"
 
-if errors.length > 0
+unless errors.empty?
   puts "There were #{errors.length} errors:"
-  errors.each { |e| print e.name + (e == errors.last ? ".\n" : ", ")}
-  puts "These probably occurred because an identically-named record already existed, or because there were multiple entries with the same name in the csv file."
+  errors.each { |e| print e.name + (e == errors.last ? ".\n" : ', ') }
+  puts 'These probably occurred because an identically-named record already \
+existed, or because there were multiple entries with the same name \
+in the CSV file.'
+  exit 1
 end
